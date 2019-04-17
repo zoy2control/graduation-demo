c7a34e63ca537569d4f5b517a4376834b2135b1e
zoy2control <paulino_zoy_it@163.com>
2019-04-17 15:14:02 +0800

    AdminController v0.1

--- zoy-graduation-web/src/main/java/com/zoy/graduation/controller/AdminController.java
+++ zoy-graduation-web/src/main/java/com/zoy/graduation/controller/AdminController.java
@@ -1,9 +1,7 @@
 package com.zoy.graduation.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by zouzp on 2019/4/16.
